9661f8c1ccd49859d9e163b709f4eeff50109b42
Sneh Nandu <snehn777@gmail.com>
2021-09-09 13:07:00 -0700

    Add files via upload

---
+++ MyStack.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class MyStack
+    {
+        private int StackSize;    
+        public int top;
+        // Implementing stack using Array
+        Object[] stack;
+
+        // Default constructor is called taking in the size of stack as parameter
+        public MyStack(int SizeOfStack)
+        {
+            stack = new Object[SizeOfStack];
+            StackSize = SizeOfStack;
+            top = -1;
+        }
+
+
+        public void Push(object element)
+        {
+            // Condition for if stack is full
+            if (top == (StackSize - 1))
+            {
+                Console.WriteLine("\nStack Overflow");
+            }
+            // If not full insert
+            else
+            {
+                stack[++top] = element;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nItem pushed successfully!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void Pop()
+        {
+            // Check if stack is empty
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is empty!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nItem Poped successfully!");
+                stack[top--] = null;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void Peek()
+        {
+            // Check if Stack is empty
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is empty!");
+            }
+            // If not empty return the top element
+            else
+            {
+                Console.WriteLine("\nTop Element is: "+ stack[top]); 
+            }
+        }
+
+        public int Size()
+        {
+            int count = 0;
+            // If Stack is empty return size = 0
+            if (top == -1)
+            {
+                return 0;
+            }
+            for (int i = top; i > -1; i--)
+            {
+                count = count + 1;
+            }
+            return count;
+        }
+
+        public bool Contains(object element)
+        {
+            Console.WriteLine();
+            // Is Stack empty return false
+            if (top == -1)
+            {
+                return false;
+            }
+            // If element in stack return true
+            for (int i = top; i > -1; i--)
+            {
+                if (stack[i].Equals(element))
+                {
+                    return true;
+                }
+            }
+            // For all other conditions return false
+            return false;
+        }
+
+        public void Reverse()
+        {
+            // Stack is empty
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is empty!");
+                return ;
+            }
+
+            object[] reverseStack = new Object[Size()];
+            int count = 0;
+            for (int i = top; i > -1; i--)
+            {
+                reverseStack[count] = stack[i];
+                count += 1;
+            }
+            for(int i = 0; i < Size(); i++)
+            {
+                stack[i] = reverseStack[i];
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nStack Reversed Successfully!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        // This method gives Iterated List form the starting index of element using IEnumerable and yeild return
+        public void Iterator()
+        {
+            
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is empty!");
+                return;
+            }
+
+
+            IEnumerable<object> myStack = GetIteratedStack();
+            Console.WriteLine("\nIterated Stack is: ");
+            foreach (var i in myStack)
+            {
+                if (i != null)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public IEnumerable<object> GetIteratedStack()
+        {
+            foreach (var items in stack)
+            {
+                // Returning the element after every iteration
+                yield return items;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is Empty");
+                return ;
+            }
+            for (int i = top; i > -1; i--)
+            {                
+              Console.WriteLine("Item {0}: {1}", (i + 1), stack[i]);                
+            }
+        }
+    }
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a stack of size 10. (Note - You can change the size of stack as per your need)
+            // For this question have taken a stack that can take only integers.
+            MyStack myStack = new MyStack(10);
+            int flag = 0,element;
+
+        redo:
+            Console.Clear();
+            ShowOptions();
+            int choice = Choice();
+            while (flag == 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        element = GetValue();
+                        myStack.Push(element);
+                        PressToContinue();
+                        goto redo;
+
+                    case 2:
+                        myStack.Pop();
+                        PressToContinue();
+                        goto redo;
+
+                    case 3:
+                        myStack.Peek();
+                        PressToContinue();
+                        goto redo;
+
+                    case 4:
+                        element = GetValue();
+                        Console.WriteLine(myStack.Contains(element));
+                        PressToContinue();
+                        goto redo;
+
+                    case 5:
+                        Console.WriteLine("\nSize is: " + myStack.Size());
+                        PressToContinue();
+                        goto redo;
+
+                    case 6:
+                        myStack.Reverse();
+                        PressToContinue();
+                        goto redo;
+
+                    case 7:
+                        myStack.Iterator();
+                        PressToContinue();
+                        goto redo;
+
+                    case 8:
+
+                        myStack.Print();
+                        PressToContinue();
+                        goto redo;
+                    // Case to exit the application
+                    case 0:
+                        flag = 1;
+                        break;
+                }
+            }
+        }
+        // Displaying operations
+        public static void ShowOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press 0 ---> Exit");
+            Console.WriteLine("Press 1 ---> Push Element");
+            Console.WriteLine("Press 2 ---> Pop Element");
+            Console.WriteLine("Press 3 ---> Peek");
+            Console.WriteLine("Press 4 ---> Contains");
+            Console.WriteLine("Press 5 ---> Size");
+            Console.WriteLine("Press 6 ---> Reverse");
+            Console.WriteLine("Press 7 ---> Iterator");
+            Console.WriteLine("Press 8 ---> Print");
+        }
+        // To get choice of operation
+        public static int Choice()
+        {
+        redo:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\nEnter Choice of Operation: ");
+            int choice;
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+                while (choice < 0 || choice > 8)
+                {
+                    Console.Write("Please enter valid choice: ");
+                    choice = int.Parse(Console.ReadLine());
+                }
+                return choice;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nError Message: Invalid Type. Please enter a valid choice");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+        // To get value
+        public static int GetValue()
+        {
+        redo:
+            try
+            {
+                Console.Write("\nEnter Value : ");
+                int value = int.Parse(Console.ReadLine());
+                return value;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid Type. Please enter a integer");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+
+        public static void PressToContinue()
+        {
+            Console.WriteLine("\nPress Any Key to continue: ");
+            Console.ReadKey();
+        }
+    }
+}
